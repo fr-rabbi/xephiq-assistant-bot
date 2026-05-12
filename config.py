39717d32477673f1a1f3e9765d5c63b5d3f564ed
Fazle Rabbi <fazle3040@gmail.com>
2026-05-12 10:14:50 +0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+MONGO_URI = os.getenv("MONGO_URI")
+OWNER_ID = 123456789
+FORCE_CHANNEL = "Xephiq"
