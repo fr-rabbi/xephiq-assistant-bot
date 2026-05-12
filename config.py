5e4af0008c7e040126d4bbc416f005da2edb4738
Fazle Rabbi <fazle3040@gmail.com>
2026-05-12 10:28:51 +0600

    Update config.py

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 MONGO_URI = os.getenv("MONGO_URI")
-OWNER_ID = 123456789
+OWNER_ID = 7222316514
 FORCE_CHANNEL = "Xephiq"
